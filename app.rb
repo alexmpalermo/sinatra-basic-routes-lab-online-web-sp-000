095aeee46c74ef1a63673daf955090991419eb29
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-26 20:21:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  get '/' do
-    "Hello, World!"
+  get '/name' do
+    "My name is __"
   end
+  
+  get '/hometown' do 
+    
   
 end
